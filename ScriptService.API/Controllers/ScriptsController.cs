56dd1892881226a26be30e853b17f1a09f280c52
Roman Krysyaev <roman-krysyaev@yandex.ru>
2023-03-05 00:55:24 +0300

    Added docker support and basic controller.

---
+++ ScriptService.API/Controllers/ScriptsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScriptService.API.Controllers
+{
+	[ApiController]
+	[Route("/api/{controller}")]
+	public class ScriptsController : Controller
+	{
+		private readonly ILogger<ScriptsController> _logger;
+
+		public ScriptsController(ILogger<ScriptsController> logger) 
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public ActionResult Get(string? filter = null)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		[Authorize]
+		public ActionResult Post(string? filter = null)
+		{
+			return Ok();
+		}
+
+		[HttpDelete]
+		[Authorize]
+		public ActionResult Delete(string? filter = null)
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		[Authorize]
+		public ActionResult Put(string? filter = null)
+		{
+			return Ok();
+		}
+	}
+}
