51dc871b8f60f2a740d76c6a99ba0840b5102313
Roman Krysyaev <roman-krysyaev@yandex.ru>
2023-03-11 21:19:32 +0300

    Docker for client can be launched now separately from docker-compose. docker build -t blazor-webassembly-with-docker . -f ./ScriptService.App/Dockerfile docker run -p 8080:80 blazor-webassembly-with-docker

---
+++ ScriptService.ComponentLibrary/Pages/ScriptsOverview.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using ScriptService.ComponentLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptService.ComponentLibrary.Pages
+{
+	public partial class ScriptsOverview
+	{
+		[Inject]
+		private IScriptManagementService _scriptManagementService { get; set; }
+
+
+	}
+}
