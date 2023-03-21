655866441142ec42a74668e023f7ddf203c3813f
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-03-21 21:55:02 +0300

    Added roles.

--- ScriptService.DataManagement/Migrations/20230321185356_AddedRoles.Designer.cs
+++ ScriptService.DataManagement/Migrations/20230321185356_AddedRoles.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ScriptService.DataManagement;
@@ -11,9 +12,11 @@ using ScriptService.DataManagement;
 namespace ScriptService.DataManagement.Migrations
 {
     [DbContext(typeof(ScriptDbContext))]
-    partial class ScriptDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230321185356_AddedRoles")]
+    partial class AddedRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ScriptService.DataManagement/ScriptDbContext.cs
+++ ScriptService.DataManagement/ScriptDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using ScriptService.DataManagement.Configuration;
 using ScriptService.Models.Data;
 using System;
 using System.Collections.Generic;
@@ -11,23 +12,24 @@ using System.Threading.Tasks;
 namespace ScriptService.DataManagement
 {
     public class ScriptDbContext : IdentityDbContext
-	{
-		private readonly IConfiguration configuration;
+    {
+        private readonly IConfiguration configuration;
 
-		private readonly DbContextOptions<ScriptDbContext> dbContextOptions;
+        private readonly DbContextOptions<ScriptDbContext> dbContextOptions;
 
-		public DbSet<Script> Scripts { get; set; }
+        public DbSet<Script> Scripts { get; set; }
 
-		public ScriptDbContext(IConfiguration configuration, DbContextOptions<ScriptDbContext> dbContextOptions) : base(dbContextOptions)
-		{
-			this.configuration = configuration;
-			this.dbContextOptions = dbContextOptions;
-		}
+        public ScriptDbContext(IConfiguration configuration, DbContextOptions<ScriptDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            this.configuration = configuration;
+            this.dbContextOptions = dbContextOptions;
+        }
 
-		protected override void OnModelCreating(ModelBuilder builder) 
-		{ 
-			base.OnModelCreating(builder);
-			builder.HasPostgresEnum<ScriptType>();
-		}
-	}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasPostgresEnum<ScriptType>();
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
+    }
 }
