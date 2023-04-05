2e89aa1539e4232dac216d74d1c475cee2311fbe
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-04-06 01:13:56 +0300

    Fixed blazor styles.

--- ScriptService.App/Program.cs
+++ ScriptService.App/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScriptService.App;
 using ScriptService.ComponentLibrary.Services;
 
@@ -9,6 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
