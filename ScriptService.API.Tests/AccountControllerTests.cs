b1fc48d4a9d08bdfaf66470c2ef21c10ae149d69
roman-krysyaev <roman-krysyaev@yandex.ru>
2023-05-10 00:52:53 +0300

    Added unit test project with placeholders.

---
+++ ScriptService.API.Tests/AccountControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptService.API.Tests
+{
+				public class AccountControllerTests
+				{		
+								[Fact]
+								public void RegisterTest()
+								{
+												Assert.Fail("So far nothing here");
+								}
+
+								[Fact]
+								public void LoginTest()
+								{
+												Assert.Fail("So far nothing here");
+								}
+				}
+}
---
+++ ScriptService.API.Tests/ScriptControllerTests.cs
@@ -0,0 +1,35 @@
+namespace ScriptService.API.Tests
+{
+				public class ScriptControllerTests
+				{
+								[Fact]
+								public void GetTest()
+								{
+												Assert.Fail("So far noting here");
+								}
+
+								[Fact]
+								public void GetByIdTest()
+								{
+												Assert.Fail("So far noting here");
+								}
+
+								[Fact]
+								public void PostTest()
+								{
+												Assert.Fail("So far noting here");
+								}
+
+								[Fact]
+								public void DeleteTest()
+								{
+												Assert.Fail("So far noting here");
+								}
+
+								[Fact]
+								public void PutTest()
+								{
+												Assert.Fail("So far noting here");
+								}
+				}
+}
